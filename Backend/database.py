2ccf3becc628cd09533d32c6f0ceeafdd369b1a2
Deepak Kumar <dk5391101@gmail.com>
2026-08-20 16:41:27 +0530

    Use MongoDB environment variable

--- Backend/database.py
+++ Backend/database.py
@@ -1,13 +1,12 @@
+import os
 from pymongo import MongoClient
 
-client = MongoClient("mongodb://localhost:27017/")
+client = MongoClient(os.getenv("MONGODB_URI"))
 
 db = client["banking_database"]
 
 accounts_collection = db["accounts"]
-
 transactions_collection = db["transactions"]
-
 users_collection = db["users"]
 
 print("MongoDB connected")
